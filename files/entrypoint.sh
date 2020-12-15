2f425e28fa864239f0b45659cd518b50403a06d5
NoxInmortus <noxinmortus@protonmail.com>
2020-12-15 12:14:16 +0100

    Cleanup apache2.conf and entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -4,21 +4,18 @@ source /etc/apache2/envvars
 
 set -euo pipefail
 
-# Setup apache2
-if [[ ! -f /etc/apache2/sites-available/000-default.conf ]]; then
+# Setup apache2 virtual host
+if [ ! -f /etc/apache2/sites-available/000-default.conf ]; then
 echo "<VirtualHost *:8080>
    ServerName ${APACHE_SERVERNAME:-$HOSTNAME}
    ServerAdmin ${APACHE_SERVERADMIN:-webmaster@localhost}
 
    DocumentRoot /usr/share/${LTB_PROJECT}/htdocs
-   DirectoryIndex index.php
+   DirectoryIndex index.php" > /etc/apache2/sites-available/000-default.conf
 
-   LogLevel warn
-   ErrorLog \${APACHE_LOG_DIR}/error.log
-   CustomLog \${APACHE_LOG_DIR}/access.log combined" > /etc/apache2/sites-available/000-default.conf
-
-if [ "${APACHE_LDAP_AUTH:-false}" == "true" ]; then
-  cat >> /etc/apache2/sites-available/000-default.conf << EOF
+# LDAP authentication
+  if [ "${APACHE_LDAP_AUTH:-false}" == "true" ]; then
+    cat >> /etc/apache2/sites-available/000-default.conf << EOF
 <Directory /usr/share/${LTB_PROJECT}/htdocs>
   AllowOverride None
   AuthType basic
@@ -32,25 +29,33 @@ if [ "${APACHE_LDAP_AUTH:-false}" == "true" ]; then
   Require ldap-group ${APACHE_AUTH_LDAP_GROUP:-cn=support,ou=groups,dc=example,dc=com}
  </Directory>
 EOF
+  fi
 
-  cat >> /etc/apache2/mods-enabled/ldap.conf << EOF
+# Misc LDAP options
+  if [ ! -f /etc/apache2/mods-enabled/ldap.conf ]; then
+    cat >> /etc/apache2/mods-enabled/ldap.conf << EOF
 LDAPTrustedGlobalCert ${APACHE_AUTH_LDAP_TRUSTED_CA:-}
 LDAPVerifyServerCert ${APACHE_AUTH_LDAP_VERIFY_CERT:-On}
 EOF
-fi
+  fi
+
 echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
 fi
 
-if [ "${APACHE_TRUST_PROXY_SSL:-false}" == "true" ]; then
-  echo 'SetEnvIf X-Forwarded-Proto "^https$" HTTPS=on' > /etc/apache2/mods-enabled/remoteip_ssl.conf
-fi
+chmod -v 0640 /etc/apache2/sites-available/000-default.conf
 
-# Display real client IPs in apache2 logs
-cat >> /etc/apache2/mods-enabled/remoteip.conf << EOF
-RemoteIPHeader X-Forwarded-For
-RemoteIPInternalProxy 127.0.0.1
-RemoteIPInternalProxy 172.16.0.0/16
+if [ ! -f /etc/apache2/mods-enabled/remoteip.conf ]; then
+  # Display real client IPs in apache2 logs
+  cat >> /etc/apache2/mods-enabled/remoteip.conf << EOF
+  RemoteIPHeader X-Forwarded-For
+  RemoteIPInternalProxy 127.0.0.1
+  RemoteIPInternalProxy 172.16.0.0/16
 EOF
+  # Let apache know we're behind a TLS reverse proxy
+  if [ "${APACHE_TRUST_PROXY_SSL:-false}" == "true" ]; then
+  echo 'SetEnvIf X-Forwarded-Proto "^https$" HTTPS=on' >> /etc/apache2/mods-enabled/remoteip.conf
+  fi
+fi
 
 source /"${LTB_PROJECT}".sh
 
